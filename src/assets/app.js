16903f7eab6efd769d6d12de621ce0f2d9b4a7ad
Trystan Eveno <trystan.eveno@lephare.com>
2018-11-28 16:43:10 +0100

    major updates

--- src/assets/app.js
+++ src/assets/app.js
@@ -11,4 +11,8 @@ import 'lazysizes/plugins/unveilhooks/ls.unveilhooks.min';
 
 domready(() => {
   lazySizes.init();
+
+  $('.carousel-indicators li').click(e => {
+    $('#carousel-home').carousel(parseInt(e.target.getAttribute('data-section-to')));
+  });
 });
